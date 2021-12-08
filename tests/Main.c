8648eeed8821f927ca5af0db395ffb720aed6064
Anders Busch <andersbusch@gmail.com>
2021-12-08 19:40:24 +0100

    fixing some formatting

--- tests/Main.c
+++ tests/Main.c
@@ -7,6 +7,6 @@ TEST_LIST = {
        { "getting minimum and maximum from tree", RBT_test_min_max },
        { "getting minimum and maximum from empty tree", RBT_test_min_max_null },
        { "deleting nodes in tree", RBT_test_remove },
-       { "static allocation", RBT_test_static_allocate_nodes},
+       { "static allocation", RBT_test_static_allocate_nodes },
        { 0 }
 };
